74aad8438b9731dc7843e7585f95ffabc0320f5a
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-05-07 23:41:21 -0400

    Implement post sorting and pin/bookmark indicators

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,22 +3,46 @@ import PostListItem from "./PostListItem";
 import PropTypes from "prop-types";
 
 const PostList = (props) => {
-  // Return the total number of comments + replies of a post
-  const countComments = (post) => {
-    return post.comments
+  // TODO: Sort posts by PINNED > BOOKMARKED > REST
+  const postTypes = {
+    pinned: [],
+    bookmarked: [],
+    default: []
+  };
+  for (const post of props.posts) {
+    if (post.pinned) {
+      postTypes.pinned.push(post);
+    } else if (post.bookmarked) {
+      postTypes.bookmarked.push(post);
+    } else {
+      postTypes.default.push(post);
+    }
+  }
+  const sortedPosts = postTypes.pinned
+    .concat(postTypes.bookmarked)
+    .concat(postTypes.default);
+  const posts = sortedPosts.map(post => {
+    // Get the total number of comments + replies of a post
+    const numComments = post.comments
       .map(comment => 1 + comment.replies.length)
       .reduce((a, b) => a + b, 0);
-  };
-  const posts = props.posts.map(post => {
+    // Check if a student has commented on the post
+    const showStudentBadge = post.comments.filter(comment => comment.author_permissions === 0).length > 0;
+    // Check if an instructor has commented on the post
+    const showInstructorBadge = post.comments.filter(comment => comment.author_permissions === 1).length > 0;
     return (
       <PostListItem
         key={post.id}
         title={post.title}
         body={post.body}
+        pinned={post.pinned}
         bookmarked={post.bookmarked}
+        bestAnswer={post.best_answer}
         tags={post.tags}
         views={post.views}
-        comments={countComments(post)}
+        comments={numComments}
+        showStudentBadge={showStudentBadge}
+        showInstructorBadge={showInstructorBadge}
       />
     );
   });
--- src/components/PostListItem.js
+++ src/components/PostListItem.js
@@ -2,28 +2,13 @@ import "./PostListItem.scss";
 import Bookmark from "./Bookmark";
 import Badge from "./Badge";
 import Button from "./Button";
+import pin from "../images/pin.png";
 import eye from "../images/eye.png";
 import comment from "../images/comment.png";
 import PropTypes from "prop-types";
 
 const PostListItem = (props) => {
-  const numViews = 45;
-  const numComments = 13;
-  const propTags = [
-    {
-      id: 1,
-      name: "HTML"
-    },
-    {
-      id: 2,
-      name: "CSS"
-    },
-    {
-      id: 3,
-      name: "React"
-    },
-  ];
-  const tags = propTags.map(tag => {
+  const tags = props.tags.map(tag => {
     return <Button disabled={true} type="tag" key={tag.id} text={tag.name} />;
   });
   return (
@@ -31,17 +16,21 @@ const PostListItem = (props) => {
       <header>
         <div>
           <span className="bookmark">
-            <Bookmark bookmarked={true || props.bookmarked} />
+            <Bookmark bookmarked={props.bookmarked} />
           </span>
-          <span className="title">PostListItem.title</span>
+          <span className="title">
+            {props.title}
+            {props.pinned && <img src={pin} alt="pin" />}
+          </span>
         </div>
         <div className="badges">
-          {true && <Badge type="instructor" />}
-          {true && <Badge type="student" />}
+          {props.showStudentBadge && <Badge type="student" />}
+          {props.showInstructorBadge && <Badge type="instructor" />}
+          {props.bestAnswer === null && <Badge type="unresolved" />}
         </div>
       </header>
       <div className="summary text-truncate">
-        {"PostListItem.body PostListItem.body PostListItem.body PostListItem.body PostListItem.body PostListItem.body PostListItem.body PostListItem.body PostListItem.body PostListItem.body "}
+        {props.body}
       </div>
       <footer>
         <div className="tags">
@@ -50,11 +39,11 @@ const PostListItem = (props) => {
         <div className="counters">
           <span className="views">
             <img src={eye} alt="views" />
-            {numViews}
+            {props.views}
           </span>
           <span className="comments">
             <img src={comment} alt="comments" />
-            {numComments}
+            {props.comments}
           </span>
         </div>
       </footer>
@@ -63,7 +52,16 @@ const PostListItem = (props) => {
 };
 
 PostListItem.propTypes = {
-  bookmarked: PropTypes.bool
+  title: PropTypes.string,
+  body: PropTypes.string,
+  bestAnswer: PropTypes.number,
+  pinned: PropTypes.bool,
+  bookmarked: PropTypes.bool,
+  tags: PropTypes.array,
+  views: PropTypes.number,
+  comments: PropTypes.number,
+  showStudentBadge: PropTypes.bool,
+  showInstructorBadge: PropTypes.bool
 };
 
 export default PostListItem;
