54c9299f9815dedddc847ae941b6395b4d320cee
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-05-08 13:12:29 -0400

    Fix App overflow

--- src/components/App.js
+++ src/components/App.js
@@ -78,6 +78,356 @@ const App = (props) => {
       ]
     },
     {
+      "id": 1,
+      "title": "How do I italicize text in HTML? 123 abc ??? 123",
+      "body": "I'm trying to italicize text but can't figure out how. Any ideas? I'm trying to italicize text but can't figure out how. Any ideas?",
+      "bookmarked": true,
+      "created_at": "1620365282",
+      "last_modified": "1620365282",
+      "best_answer": 1,
+      "author_name": "FirstName LastName",
+      "tags": [
+        {
+          "id": 1,
+          "name": "general"
+        },
+        {
+          "id": 8,
+          "name": "html"
+        }
+      ],
+      "pinned": true,
+      "views": 9,
+      "comments": [
+        {
+          "id": 1,
+          "anonymous": false,
+          "author_name": "Willy Wonka",
+          "author_avatar_url": "avatar.png",
+          "author_permissions": 0,
+          "body": "Try <i>text</i>.",
+          "score": 7,
+          "created_at": "1620365282",
+          "last_modified": "1620365282",
+          "endorsed": true,
+          "replies": [
+            {
+              "id": 2,
+              "anonymous": false,
+              "author_name": "Jane Doe",
+              "author_avatar_url": "avatar.png",
+              "body": "good answer!",
+              "created_at": "1620365282",
+              "last_modified": "1620365282"
+            }
+          ]
+        },
+        {
+          "id": 3,
+          "anonymous": false,
+          "author_name": "Milly Monka",
+          "author_avatar_url": "avatar.png",
+          "author_permissions": 1,
+          "body": "Use <b>text</b>.",
+          "score": 1,
+          "created_at": "1620365282",
+          "last_modified": "1620365282",
+          "endorsed": false,
+          "replies": [
+            {
+              "id": 4,
+              "anonymous": true,
+              "author_name": "John Doe",
+              "author_avatar_url": "anonymous.png",
+              "body": "this doesn't work!",
+              "created_at": "1620365282",
+              "last_modified": "1620365282"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 1,
+      "title": "How do I italicize text in HTML? 123 abc ??? 123",
+      "body": "I'm trying to italicize text but can't figure out how. Any ideas? I'm trying to italicize text but can't figure out how. Any ideas?",
+      "bookmarked": true,
+      "created_at": "1620365282",
+      "last_modified": "1620365282",
+      "best_answer": 1,
+      "author_name": "FirstName LastName",
+      "tags": [
+        {
+          "id": 1,
+          "name": "general"
+        },
+        {
+          "id": 8,
+          "name": "html"
+        }
+      ],
+      "pinned": true,
+      "views": 9,
+      "comments": [
+        {
+          "id": 1,
+          "anonymous": false,
+          "author_name": "Willy Wonka",
+          "author_avatar_url": "avatar.png",
+          "author_permissions": 0,
+          "body": "Try <i>text</i>.",
+          "score": 7,
+          "created_at": "1620365282",
+          "last_modified": "1620365282",
+          "endorsed": true,
+          "replies": [
+            {
+              "id": 2,
+              "anonymous": false,
+              "author_name": "Jane Doe",
+              "author_avatar_url": "avatar.png",
+              "body": "good answer!",
+              "created_at": "1620365282",
+              "last_modified": "1620365282"
+            }
+          ]
+        },
+        {
+          "id": 3,
+          "anonymous": false,
+          "author_name": "Milly Monka",
+          "author_avatar_url": "avatar.png",
+          "author_permissions": 1,
+          "body": "Use <b>text</b>.",
+          "score": 1,
+          "created_at": "1620365282",
+          "last_modified": "1620365282",
+          "endorsed": false,
+          "replies": [
+            {
+              "id": 4,
+              "anonymous": true,
+              "author_name": "John Doe",
+              "author_avatar_url": "anonymous.png",
+              "body": "this doesn't work!",
+              "created_at": "1620365282",
+              "last_modified": "1620365282"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 1,
+      "title": "How do I italicize text in HTML? 123 abc ??? 123",
+      "body": "I'm trying to italicize text but can't figure out how. Any ideas? I'm trying to italicize text but can't figure out how. Any ideas?",
+      "bookmarked": true,
+      "created_at": "1620365282",
+      "last_modified": "1620365282",
+      "best_answer": 1,
+      "author_name": "FirstName LastName",
+      "tags": [
+        {
+          "id": 1,
+          "name": "general"
+        },
+        {
+          "id": 8,
+          "name": "html"
+        }
+      ],
+      "pinned": true,
+      "views": 9,
+      "comments": [
+        {
+          "id": 1,
+          "anonymous": false,
+          "author_name": "Willy Wonka",
+          "author_avatar_url": "avatar.png",
+          "author_permissions": 0,
+          "body": "Try <i>text</i>.",
+          "score": 7,
+          "created_at": "1620365282",
+          "last_modified": "1620365282",
+          "endorsed": true,
+          "replies": [
+            {
+              "id": 2,
+              "anonymous": false,
+              "author_name": "Jane Doe",
+              "author_avatar_url": "avatar.png",
+              "body": "good answer!",
+              "created_at": "1620365282",
+              "last_modified": "1620365282"
+            }
+          ]
+        },
+        {
+          "id": 3,
+          "anonymous": false,
+          "author_name": "Milly Monka",
+          "author_avatar_url": "avatar.png",
+          "author_permissions": 1,
+          "body": "Use <b>text</b>.",
+          "score": 1,
+          "created_at": "1620365282",
+          "last_modified": "1620365282",
+          "endorsed": false,
+          "replies": [
+            {
+              "id": 4,
+              "anonymous": true,
+              "author_name": "John Doe",
+              "author_avatar_url": "anonymous.png",
+              "body": "this doesn't work!",
+              "created_at": "1620365282",
+              "last_modified": "1620365282"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 1,
+      "title": "How do I italicize text in HTML? 123 abc ??? 123",
+      "body": "I'm trying to italicize text but can't figure out how. Any ideas? I'm trying to italicize text but can't figure out how. Any ideas?",
+      "bookmarked": true,
+      "created_at": "1620365282",
+      "last_modified": "1620365282",
+      "best_answer": 1,
+      "author_name": "FirstName LastName",
+      "tags": [
+        {
+          "id": 1,
+          "name": "general"
+        },
+        {
+          "id": 8,
+          "name": "html"
+        }
+      ],
+      "pinned": true,
+      "views": 9,
+      "comments": [
+        {
+          "id": 1,
+          "anonymous": false,
+          "author_name": "Willy Wonka",
+          "author_avatar_url": "avatar.png",
+          "author_permissions": 0,
+          "body": "Try <i>text</i>.",
+          "score": 7,
+          "created_at": "1620365282",
+          "last_modified": "1620365282",
+          "endorsed": true,
+          "replies": [
+            {
+              "id": 2,
+              "anonymous": false,
+              "author_name": "Jane Doe",
+              "author_avatar_url": "avatar.png",
+              "body": "good answer!",
+              "created_at": "1620365282",
+              "last_modified": "1620365282"
+            }
+          ]
+        },
+        {
+          "id": 3,
+          "anonymous": false,
+          "author_name": "Milly Monka",
+          "author_avatar_url": "avatar.png",
+          "author_permissions": 1,
+          "body": "Use <b>text</b>.",
+          "score": 1,
+          "created_at": "1620365282",
+          "last_modified": "1620365282",
+          "endorsed": false,
+          "replies": [
+            {
+              "id": 4,
+              "anonymous": true,
+              "author_name": "John Doe",
+              "author_avatar_url": "anonymous.png",
+              "body": "this doesn't work!",
+              "created_at": "1620365282",
+              "last_modified": "1620365282"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 1,
+      "title": "How do I italicize text in HTML? 123 abc ??? 123",
+      "body": "I'm trying to italicize text but can't figure out how. Any ideas? I'm trying to italicize text but can't figure out how. Any ideas?",
+      "bookmarked": true,
+      "created_at": "1620365282",
+      "last_modified": "1620365282",
+      "best_answer": 1,
+      "author_name": "FirstName LastName",
+      "tags": [
+        {
+          "id": 1,
+          "name": "general"
+        },
+        {
+          "id": 8,
+          "name": "html"
+        }
+      ],
+      "pinned": true,
+      "views": 9,
+      "comments": [
+        {
+          "id": 1,
+          "anonymous": false,
+          "author_name": "Willy Wonka",
+          "author_avatar_url": "avatar.png",
+          "author_permissions": 0,
+          "body": "Try <i>text</i>.",
+          "score": 7,
+          "created_at": "1620365282",
+          "last_modified": "1620365282",
+          "endorsed": true,
+          "replies": [
+            {
+              "id": 2,
+              "anonymous": false,
+              "author_name": "Jane Doe",
+              "author_avatar_url": "avatar.png",
+              "body": "good answer!",
+              "created_at": "1620365282",
+              "last_modified": "1620365282"
+            }
+          ]
+        },
+        {
+          "id": 3,
+          "anonymous": false,
+          "author_name": "Milly Monka",
+          "author_avatar_url": "avatar.png",
+          "author_permissions": 1,
+          "body": "Use <b>text</b>.",
+          "score": 1,
+          "created_at": "1620365282",
+          "last_modified": "1620365282",
+          "endorsed": false,
+          "replies": [
+            {
+              "id": 4,
+              "anonymous": true,
+              "author_name": "John Doe",
+              "author_avatar_url": "anonymous.png",
+              "body": "this doesn't work!",
+              "created_at": "1620365282",
+              "last_modified": "1620365282"
+            }
+          ]
+        }
+      ]
+    },
+    {
       "id": 2,
       "title": "Where can I find the prep work?",
       "body": "I checked everywhere but can't find the link.",
@@ -460,8 +810,12 @@ const App = (props) => {
     <main className="App">
       <Nav />
       <section>
-        <PostList posts={posts} />
-        <Main />
+        <div className="left">
+          <PostList posts={posts} />
+        </div>
+        <div className="right">
+          <Main />
+        </div>
       </section>
     </main>
   );
