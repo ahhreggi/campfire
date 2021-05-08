e9ed3736b20eb64d96da3ad48618f9a76ddf343b
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-05-07 22:35:51 -0400

    Add countComments

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,15 +1,36 @@
 import "./PostList.scss";
 import PostListItem from "./PostListItem";
+import PropTypes from "prop-types";
 
 const PostList = (props) => {
+  // Return the total number of comments + replies of a post
+  const countComments = (post) => {
+    return post.comments
+      .map(comment => 1 + comment.replies.length)
+      .reduce((a, b) => a + b, 0);
+  };
+  const posts = props.posts.map(post => {
+    return (
+      <PostListItem
+        key={post.id}
+        title={post.title}
+        body={post.body}
+        bookmarked={post.bookmarked}
+        tags={post.tags}
+        views={post.views}
+        comments={countComments(post)}
+      />
+    );
+  });
   return (
     <div className="PostList">
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
+      {posts}
     </div>
   );
+};
+
+PostList.propTypes = {
+  posts: PropTypes.array
 };
 
 export default PostList;
