efee80195894c523cb7e8cca38a910777983fd14
Martin Halas <echoes.time@gmail.com>
2021-05-10 01:36:09 +0000

    Cleaned up stories folder. Fixed css for nav story. TODO: Add rest of stories, configure .storybook config to always include ~/src/index.scss

--- src/stories/Nav.stories.js
+++ src/stories/Nav.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Nav from '../components/Nav';
+import '../index.scss';
 
 export default {
   title: 'Nav-bar',
@@ -14,6 +15,6 @@ LoggedIn.args = {
   active: true,
   viewTitle: 'Hello',
   courseName: 'March 1st 2021',
-  userAvatar: 'Placeholder :-)',
+  userAvatar: '../src/images/avatars/2.png',
   userName: 'LeetHax0r'
 };
--- src/stories/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Fragment } from "react";
-
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-
-import "index.scss";
-
-storiesOf("Name", module)
