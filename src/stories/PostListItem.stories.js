eec6599d4b48f7a2ae9e47ec20ffb57eb193ca56
Martin Halas <echoes.time@gmail.com>
2021-05-10 21:02:46 +0000

    Added postlistitem stories, tags not working properly.

--- src/stories/PostListItem.stories.js
+++ src/stories/PostListItem.stories.js
@@ -4,13 +4,46 @@ import PostListItem from '../components/PostListItem';
 
 export default {
   title: 'PostListItem',
-  component: PostListItem
+  component: PostListItem,
+  argTypes: { onClick: { action: 'Clicked' }},
+  decorators: [(Story) => <div style={{ width: '20rem'}}><Story/></div>]
 };
 
 const Template = (args) => <PostListItem {...args} />;
 
-export const TestComponent = Template.bind ({});
+export const Primary = Template.bind ({});
 
-TestComponent.args = {
+Primary.args = {
+  title: "How fix code? :(",
+  body: "My code is broken and I just don't know what to doooo",
+  tags: ["PROMISES", "CLASSES"],
+  views: 0,
+  comments: 0,
+  showStudentBadge: true
+};
 
+export const Pinned = Template.bind ({});
+
+Pinned.args = {
+  title: "Super important announcement",
+  body: "Starting tomorrow we are going to write better unit tests",
+  tags: ["PROMISES"],
+  pinned: true,
+  views: 250,
+  comments: 20,
+  showInstructorBadge: true
+};
+
+export const Bookmarked = Template.bind ({});
+
+Bookmarked.args = {
+  title: "Confused about callbacks",
+  body: "Can somebody please explain what the event loop even is because I just feel like I'm going in circles",
+  tags: ["PROMISES"],
+  bookmarked: true,
+  bestAnswer: 2,
+  views: 33,
+  comments: 2,
+  showStudentBadge: true,
+  showInstructorBadge: true
 };
