f512d4f12afb2ba8c3573538a03af5afc8fb7fcb
Martin Halas <echoes.time@gmail.com>
2021-05-10 23:55:09 +0000

    Added postform stories for posts with and without text

--- src/stories/PostForm.stories.js
+++ src/stories/PostForm.stories.js
@@ -4,13 +4,23 @@ import PostForm from '../components/PostForm';
 
 export default {
   title: 'PostForm',
-  component: PostForm
+  component: PostForm,
+  argTypes: { onChange: { action: 'Changed' }},
+  decorators: [(Story) => <div style={{ 'margin-left': '20rem'}}><Story/></div>]
 };
 
 const Template = (args) => <PostForm {...args} />;
 
-export const TestComponent = Template.bind ({});
+export const Primary = Template.bind ({});
+Primary.storyName = 'Form with Text'
+Primary.args = {
+  label: "How do I lorem ipsum my Javascript?",
+  text: "I would like to figure out how to lorem ipsum my javascript but it's hard for me to figure out the whole latin thing"
+};
 
-TestComponent.args = {
-
+export const Secondary = Template.bind ({});
+Secondary.storyName = 'Form with no Text'
+Secondary.args = {
+  label: "How do I lorem ipsum my Javascript?",
+  text: ""
 };
