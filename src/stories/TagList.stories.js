415846b7ac2547d23a9dd9fe557e08d6cecb25e5
Martin Halas <echoes.time@gmail.com>
2021-05-10 23:29:08 +0000

    Added taglist stories. Used Tags dummy data from app.js

--- src/stories/TagList.stories.js
+++ src/stories/TagList.stories.js
@@ -2,6 +2,33 @@ import React from 'react';
 
 import TagList from '../components/TagList';
 
+const tags = [
+  {
+    "id": 1,
+    "name": "Callbacks"
+  },
+  {
+    "id": 2,
+    "name": "Closures"
+  },
+  {
+    "id": 3,
+    "name": "Promises"
+  },
+  {
+    "id": 4,
+    "name": "Classes"
+  },
+  {
+    "id": 5,
+    "name": "Async"
+  },
+  {
+    "id": 6,
+    "name": "HTML"
+  }
+]
+
 export default {
   title: 'TagList',
   component: TagList
@@ -9,8 +36,26 @@ export default {
 
 const Template = (args) => <TagList {...args} />;
 
-export const TestComponent = Template.bind ({});
+export const Primary = Template.bind ({});
+Primary.storyName = 'None selected'
 
-TestComponent.args = {
+Primary.args = {
+  tags,
+  selectedTags: []
+};
 
+export const Secondary = Template.bind ({});
+Secondary.storyName = 'One tag selected'
+
+Secondary.args = {
+  tags,
+  selectedTags: [tags[3]]
+};
+
+export const Tertiary = Template.bind ({});
+Tertiary.storyName = 'Multiple tags selected'
+
+Tertiary.args = {
+  tags,
+  selectedTags: [tags[0], tags[1], tags[4]]
 };
