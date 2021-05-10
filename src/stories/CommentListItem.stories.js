35bb91c65c83f3f9094051144d019878f9283cc2
Martin Halas <echoes.time@gmail.com>
2021-05-10 18:46:32 +0000

    Added stories for CommentListItem. Consistency between CommentListItem props and CommentList props should be looked at

--- src/stories/CommentListItem.stories.js
+++ src/stories/CommentListItem.stories.js
@@ -4,13 +4,47 @@ import CommentListItem from '../components/CommentListItem';
 
 export default {
   title: 'CommentListItem',
-  component: CommentListItem
+  component: CommentListItem,
+  argTypes: { onClick: { action: 'Clicked' }, props: { onEdit: {action: 'Saved!' }}}
 };
 
 const Template = (args) => <CommentListItem {...args} />;
 
-export const TestComponent = Template.bind ({});
+export const Primary = Template.bind ({});
+Primary.storyname = 'No replies, non-anonymous'
+Primary.args = {
+  id: 1,
+  anonymous: false,
+  author: "Ben Scrivens",
+  body: "Hello friends! Does anyone have any idea why my code keeps breaking?",
+  createdAt: Date.now(),
+  lastModified: Date.now(),
+  editable: false,
+  endorsable: false,
+  endorsements: [],
+  replies: []
+};
 
-TestComponent.args = {
+export const Secondary = Template.bind ({});
+Secondary.storyname = 'No replies, anonymous'
 
+Secondary.args = {
+  ...Primary.args,
+  anonymous: true,
+  author: "Billy Jean",
+  body: "Have you tried using Vue instead?",
+  editable: true,
+  endorsable: true,
+};
+
+export const Third = Template.bind ({});
+Third.storyname = 'Comment with replies'
+
+Third.args = {
+  ...Primary.args,
+  replies:
+  [
+    {...Secondary.args},
+    {...Secondary.args, body: "Actually that'd be a bad idea nvm"}
+  ]
 };
