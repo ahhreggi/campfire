fb0a9ceed3ebfd07412460605f2563765af0d97d
Martin Halas <echoes.time@gmail.com>
2021-05-10 23:30:54 +0000

    Fixed global template state by removing attributes. Stories work properly now

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -8,23 +8,28 @@ export default {
   argTypes: { onClick: { action: 'Clicked' }}
 };
 
-const Template = (args) => <Button {...args} text='Click Me!' disabled={false} />;
+const Template = (args) => <Button {...args} />;
 
 export const FormGreen = Template.bind ({});
 
 FormGreen.args = {
+  text: "Click me!",
   styles: "form green",
+  disabled: false
 };
 
 export const FormRed = Template.bind ({});
 
 FormRed.args = {
+  text: "Click me!",
   styles: "form red",
+  disabled: false
 };
 
 export const FormGreenDisabled = Template.bind ({});
 
 FormGreenDisabled.args = {
+  text: "Click me!",
   styles: "form green",
   disabled: true
 };
@@ -32,6 +37,7 @@ FormGreenDisabled.args = {
 export const FormRedDisabled = Template.bind ({});
 
 FormRedDisabled.args = {
+  text: "Click me!",
   styles: "form red",
   disabled: true
 };
@@ -40,28 +46,32 @@ export const Tag = Template.bind ({});
 
 Tag.args = {
   text: "PROMISES",
-  styles: "tag"
+  styles: "tag",
+  disabled: false
 };
 
 export const TagSelected = Template.bind ({});
 
 TagSelected.args = {
   text: "PROMISES",
-  styles: "tag selected"
+  styles: "tag selected",
+  disabled: false
 };
 
 export const TagFilter = Template.bind ({});
 
 TagFilter.args = {
   text: "PROMISES",
-  styles: "tag filter"
+  styles: "tag filter",
+  disabled: false
 };
 
 export const TagClear = Template.bind ({});
 
 TagClear.args = {
   text: "PROMISES",
-  styles: "tag clear"
+  styles: "tag clear",
+  disabled: false
 };
 
 export const TagDisabled = Template.bind ({});
