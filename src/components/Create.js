2e21a0759a4ae69945ee7d8841c6c5ed9e963f1e
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-05-12 03:15:07 -0400

    Add Create and Join components

---
+++ src/components/Create.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import TextForm from "./EditForm/TextForm";
+import Button from "./Button";
+import "./Create.scss";
+
+const Create = (props) => {
+
+  Create.propTypes = {
+    onSubmit: PropTypes.func
+  };
+
+  const [state, setState] = useState({
+    name: null,
+    description: null
+  });
+
+  const handleInputChange = (event, field) => {
+    setState({ ...state, [field]: event.target.value })
+  };
+
+  const handleSubmit = () => {
+    const data = { ...state };
+    props.onSubmit(data);
+  };
+
+  return (
+    <div className="Create">
+
+      {/* Page Title */}
+      <div className="page-title">
+        Create Page
+      </div>
+
+      {/* Form Fields */}
+      <div className="form-fields">
+        <TextForm
+          label={"Course Name"}
+          text={""}
+          onChange={(event) => handleInputChange(event, "name")}
+        />
+
+        <TextForm
+          label={"Last Name"}
+          text={""}
+          onChange={(event) => handleInputChange(event, "description")}
+        />
+
+      </div>
+
+      {/* Submit Button */}
+      <div className="submit">
+        <Button
+          text="Create"
+          styles="submit create"
+          onClick={handleSubmit}
+        />
+      </div>
+
+    </div>
+  );
+};
+
+export default Create;
---
+++ src/components/Join.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import TextForm from "./EditForm/TextForm";
+import Button from "./Button";
+import "./Join.scss";
+
+const Join = (props) => {
+
+  Join.propTypes = {
+    onSubmit: PropTypes.func
+  };
+
+  const [state, setState] = useState({
+    accessCode: null
+  });
+
+  const handleInputChange = (event, field) => {
+    setState({ ...state, [field]: event.target.value })
+  };
+
+  const handleSubmit = () => {
+    const data = { ...state };
+    props.onSubmit(data);
+  };
+
+  return (
+    <div className="Join">
+
+      {/* Page Title */}
+      <div className="page-title">
+        Join Page
+      </div>
+
+      {/* Form Fields */}
+      <div className="form-fields">
+        <TextForm
+          label={"Course Access Code"}
+          text={""}
+          onChange={(event) => handleInputChange(event, "accessCode")}
+        />
+
+      </div>
+
+      {/* Submit Button */}
+      <div className="submit">
+        <Button
+          text="Join"
+          styles="submit join"
+          onClick={handleSubmit}
+        />
+      </div>
+
+    </div>
+  );
+};
+
+export default Join;
