7fef4e4e24b00d08f4ddab22c6175e13a645fb60
Martin Halas <echoes.time@gmail.com>
2021-05-12 18:15:28 +0000

    Commented out file, needs to be refactored since component was restructured

--- src/stories/PostForm.stories.js
+++ src/stories/PostForm.stories.js
@@ -1,26 +1,26 @@
-import React from 'react';
+// import React from 'react';
 
-import PostForm from '../components/PostForm';
+// import PostForm from '../components/PostForm';
 
-export default {
-  title: 'PostForm',
-  component: PostForm,
-  argTypes: { onChange: { action: 'Changed' }},
-  decorators: [(Story) => <div style={{ 'margin-left': '20rem'}}><Story/></div>]
-};
+// export default {
+//   title: 'PostForm',
+//   component: PostForm,
+//   argTypes: { onChange: { action: 'Changed' }},
+//   decorators: [(Story) => <div style={{ 'margin-left': '20rem'}}><Story/></div>]
+// };
 
-const Template = (args) => <PostForm {...args} />;
+// const Template = (args) => <PostForm {...args} />;
 
-export const Primary = Template.bind ({});
-Primary.storyName = 'Form with Text'
-Primary.args = {
-  label: "How do I lorem ipsum my Javascript?",
-  text: "I would like to figure out how to lorem ipsum my javascript but it's hard for me to figure out the whole latin thing"
-};
+// export const Primary = Template.bind ({});
+// Primary.storyName = 'Form with Text'
+// Primary.args = {
+//   label: "How do I lorem ipsum my Javascript?",
+//   text: "I would like to figure out how to lorem ipsum my javascript but it's hard for me to figure out the whole latin thing"
+// };
 
-export const Secondary = Template.bind ({});
-Secondary.storyName = 'Form with no Text'
-Secondary.args = {
-  label: "How do I lorem ipsum my Javascript?",
-  text: ""
-};
+// export const Secondary = Template.bind ({});
+// Secondary.storyName = 'Form with no Text'
+// Secondary.args = {
+//   label: "How do I lorem ipsum my Javascript?",
+//   text: ""
+// };
