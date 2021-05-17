f7767935072fc0847c5957571ff0b6b1a72756af
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-05-17 12:30:21 -0400

    Add course counters to Manage

--- src/components/Manage.js
+++ src/components/Manage.js
@@ -15,9 +15,38 @@ const Manage = (props) => {
     onRedirect: PropTypes.func
   };
 
+  const numActiveCourses = props.userCourses.filter(course => !course.archived).length;
+  const numArchivedCourses = props.userCourses.length - numActiveCourses;
+
   return (
     <div className="Manage">
 
+      {/* Page Title */}
+      <div className="page-title">
+        Manage enrolments
+      </div>
+
+      <hr />
+
+      {/* Page Text */}
+      <div className="page-text">
+        <div>
+          You may unenrol from a course at any time, however please note that any contributions you&apos;ve made will remain unchanged (you can always delete them manually).
+        </div>
+        <div className="counters">
+          You are currently enrolled in <span className="number active">{numActiveCourses}</span> active course{numActiveCourses !== 1 ? "s" : ""}
+          {numArchivedCourses > 0 ? <> and <span className="number archived">{numArchivedCourses}</span> archived course{numArchivedCourses !== 1 ? "s" : ""}.</> : ""}
+        </div>
+      </div>
+
+      {/* CourseList */}
+      <div className="user-courses">
+        User courses here!
+      </div>
+
+
+      {/* Back to Home */}
+      <BackButton onRedirect={props.onRedirect} />
     </div>
   );
 };
