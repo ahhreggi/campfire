9f076852ba18c1481e7e9338674b35ba1f001d4e
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-05-17 13:36:05 -0400

    Add manage course button

--- src/components/ManageListItem.js
+++ src/components/ManageListItem.js
@@ -40,20 +40,32 @@ const ManageListItem = (props) => {
       <div className="view-button">
         <Button
           text={"VIEW"}
-          styles={"form green"}
+          styles={"form green manage"}
           // may have to change redirect to a course manage page later
           onClick={() => props.onViewCourse(props.id, null, null, "Dashboard")}
         />
       </div>
-      <div className={`leave-button ${props.role}`}>
-        <Button
-          text={"UNENROLL"}
-          styles={"form red"}
-          onClick={props.role !== "owner" ? () => toggleConfirmation() : null}
-          disabled={props.role === "owner"}
-        />
-      </div>
-      {state.showConfirmation &&
+      {props.role === "owner" &&
+        <div className="manage-button">
+          <Button
+            text={"MANAGE"}
+            styles="form yellow manage"
+            onClick={props.role === "owner" ? () => props.onViewCourse(props.id, null, null, "Course Settings") : null}
+            disabled={props.role !== "owner"}
+          />
+        </div>
+      }
+      {props.role !== "owner" &&
+        <div className="leave-button">
+          <Button
+            text={"UNENROLL"}
+            styles="form red manage"
+            onClick={props.role !== "owner" ? () => toggleConfirmation() : null}
+            disabled={props.role === "owner"}
+          />
+        </div>
+      }
+      {state.showConfirmation && props.role !== "owner" &&
         <div className="confirmation">
           <Confirmation
             message={`Are you sure you would like to unenroll from ${props.courseCode}?`}
